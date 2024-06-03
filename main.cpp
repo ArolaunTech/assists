7fb51791590cb0dc37175d812320c3ba9cd9b606
ArolaunTech <aadityasenji@gmail.com>
2024-06-03 16:10:31 -0700

    Restart with GLFW?

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-
-int main() {
-	std::cout << "hello world\n";
-	SDL_Init(SDL_INIT_VIDEO);
-	return 0;
-}
--- makefile
+++
@@ -1,2 +0,0 @@
-assists:
-	g++ main.cpp -o assists -I include -l SDL2-2.0.0

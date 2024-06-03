185c01a87f64380902ea3bc3caeccde391576342
ArolaunTech <aadityasenji@gmail.com>
2024-06-03 14:45:01 -0700

    Simple hello world program for now, it actually runs (at least on MacOS)

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main() {
+	std::cout << "hello world\n";
+	SDL_Init(SDL_INIT_VIDEO);
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+assists:
+	g++ main.cpp -o assists -I include -l SDL2-2.0.0

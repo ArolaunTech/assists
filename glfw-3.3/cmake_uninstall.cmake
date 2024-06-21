e3838f02243dde6fb99622ec57127bad4e40ef46
ArolaunTech <aadityasenji2@gmail.com>
2024-06-21 16:50:36 -0700

    Added glfw 3.3 for Linux support on older versions

--- glfw-3.3/cmake_uninstall.cmake
+++ glfw-3.3/cmake_uninstall.cmake
@@ -1,9 +1,9 @@
 
-if (NOT EXISTS "/home/aaditya/assists/glfw-3.4/install_manifest.txt")
-    message(FATAL_ERROR "Cannot find install manifest: \"/home/aaditya/assists/glfw-3.4/install_manifest.txt\"")
+if (NOT EXISTS "/home/aaditya/assists/glfw-3.3/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: \"/home/aaditya/assists/glfw-3.3/install_manifest.txt\"")
 endif()
 
-file(READ "/home/aaditya/assists/glfw-3.4/install_manifest.txt" files)
+file(READ "/home/aaditya/assists/glfw-3.3/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
